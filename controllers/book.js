216dfa517d9c9375fc0ff089469cfad66913d1e2
Lévy ZIRE <levy.zire.ext@francetv.fr>
2024-10-07 17:09:24 +0200

    Finish CRUD books

--- controllers/book.js
+++ controllers/book.js
@@ -16,7 +16,65 @@ module.exports = {
                 message: error.message || 'Something Wrong'
             });
         }
-    }
+    },
 
+    findAll: (req, res) => {
+        BooModel
+            .find()
+            .then((books) => {
+                res.send(books)
+            })
+            .catch((error) => {
+                res.status(500).send({
+                    message: error.message
+                })
+            })
+    },
+
+    findBook: (req, res) => {
+        const bookId = req.params.id;
+        BooModel.findById(bookId)
+            .then((book) => {
+                res.send(book);
+            })
+            .catch((error) => {
+                res.status(500).send(error.message || `Cannot find book with id=${bookId}`)
+            })
+    },
+
+    updateBook: async (req, res) => {
+        const bookId = req.params.id;
+        const book = await BooModel.findById(bookId);
+        if (!book) {
+            throw new Error('Cannot find book to update')
+        }
+        const newBook = {...book, ...req.body}
+
+        verifyBook(newBook);
+        const { name, description } = newBook;
+        BooModel.findByIdAndUpdate(bookId, {
+            name,
+            description
+        }, { new: true })
+            .then((updateBook) => {
+                res.send(updateBook)
+            })
+            .catch((error) => {
+                res.status(500).send(error.message || `Cannot update book with id=${bookId}`)
+            })
+    },
+
+    deleteBook: (req, res) => {
+        const bookId = req.params.id;
+        BooModel.findByIdAndDelete(bookId)
+            .then(book => {
+                res.send({
+                    message: 'Book was successfully delete',
+                })
+            })
+            .catch((error) => {
+                res.status(500).send(error.message || `Cannot delete book with id=${bookId}`)
+            })
+    }
 
 }
--- routes/book.js
+++ routes/book.js
@@ -1,43 +1,14 @@
 const router = require('express').Router();
 const bookController = require('./../controllers/book');
 
-let books = [
-    {
-        id: 1,
-        name: "Livre",
-        description: "description"
-    },
-    {
-        id: 2,
-        name: "Livr2",
-        description: "description2"
-    }
-]
+router.get('/', bookController.findAll);
 
-router.get('/', (req, res ) => {
-    res.send(books);
-});
-
-router.get('/:id', (req, res ) => {
-    const bookId = +req.params.id
-    const book = books.find(book => book.id === bookId);
-    res.send(book);
-});
+router.get('/:id', bookController.findBook);
 
 router.post('/', bookController.create);
 
-router.put('/', (req, res) => {
-    const newBook = req.body;
-    books = books.map((book) => {
-        return book.id === newBook.id ? newBook : book
-    })
-    res.status(201).send(books);
-})
+router.put('/:id', bookController.updateBook);
 
-router.delete('/', (req, res) => {
-    const bookId = +req.params.id;
-    books = books.filter(book => book.id !== bookId);
-    res.status(204).send();
-})
+router.delete('/:id', bookController.deleteBook);
 
 module.exports = router;
