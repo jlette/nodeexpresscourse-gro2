63bf4658229c6dd66d7482be6eba7f42454ae729
Lévy ZIRE <levy.zire.ext@francetv.fr>
2024-10-08 11:12:23 +0200

    add login function in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,78 @@
 const UserModel = require('./../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {verifyUser} = require("../validator/user");
 
 module.exports = {
-    // POST / Creer un utilisateur
+    // POST /register Creer un utilisateur
     register: async (req, res) => {
-        verifyUser(req.body);
-        const {firstname, lastname, email, password} = req.body;
-        const hash = await bcrypt.hash(password, 10);
-        const newUser = new UserModel({
-            firstname,
-            lastname,
-            email,
-            hash
-        });
+        try {
+            verifyUser(req.body);
+            const {firstname, lastname, email, password} = req.body;
+            const hash = await bcrypt.hash(password, 10);
+            const newUser = new UserModel({
+                firstname,
+                lastname,
+                email, // email: email
+                password: hash
+            });
 
-        newUser.save()
-        res.status(201).send({
-            id: newUser._id,
-            lastname: newUser.lastname,
-            firstname: newUser.firstname,
-            email: newUser.email
+            newUser.save()
+            res.status(201).send({
+                id: newUser._id,
+                lastname: newUser.lastname,
+                firstname: newUser.firstname,
+                email: newUser.email
+            })
+        } catch (error) {
+            res.send({
+                message: error.message || 'Cannot register User'
+            })
+        }
+    },
+
+    // POST /login Authentification
+    login: async  (req, res) => {
+        const {email, password} = req.body
+        const user = UserModel.findOne({
+            where: {
+                email,
+            }
         })
 
+        if(!user) {
+            res.status(401).send({
+                messsage: "User not exist"
+            })
+        }
+
+        const checkPassword = bcrypt.compare(password, user.password);
+        if (checkPassword) {
+            //prepration des infos de jwt
+            const jwtOptions = {
+                expiresIn: process.env.JWT_TIMOEOUTE_DURATION || "1h"
+            }
+            const secret = process.env.JWT_SECRET || 'secret';
+
+            //generation du token jwt
+            const token = jwt.sign({
+                userId: user.id,
+            }, secret, jwtOptions)
+
+            res.send({
+                message: 'Login successfully',
+                user: {
+                    id: user.id,
+                    firstname: user.firstname,
+                    lastname: user.lastname,
+                    token
+                }
+            })
+
+        } else {
+            res.status(401).send({
+                messsage: "Wrong login informations"
+            })
+        }
     }
 }
